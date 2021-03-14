82797f56b0015d183b03cc459c42cacc0c48cb61
LOOK4869 <80631350+LOOK4869@users.noreply.github.com>
2021-03-14 22:46:14 +0800

    Add files via upload

---
+++ Calculate1/Calculate1/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculate1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("请输入第一个数字：");
+            string a = Console.ReadLine();
+            double c = Convert.ToDouble(a);
+            Console.WriteLine("请输入第二个数字：");
+            string b = Console.ReadLine();
+            double d = Convert.ToDouble(b);
+            Console.WriteLine("请输入运算符：");
+            string e = Console.ReadLine();
+            double result;
+            if (e == "+")
+            {
+                 result = c + d;
+                Console.WriteLine("运算结果是：" + result);
+            }
+            else if(e=="-")
+            {
+               result = c - d;
+                Console.WriteLine("运算结果是：" + result);
+            }
+            else if (e == "*")
+            {
+                result = c * d;
+                Console.WriteLine("运算结果是：" + result);
+            }
+            else if (e == "/")
+            {
+                result = c / d;
+                Console.WriteLine("运算结果是：" + result);
+            }
+            else if (e == "%")
+            {
+                result = c % d;
+                Console.WriteLine("运算结果是：" + result);
+            }
+            else 
+            {
+                Console.WriteLine("没有这个运算符");
+            }
+
+            Console.ReadKey();
+
+
+
+
+        }
+    }
+}
---
+++ Calculate2/Calculate2/Form1.Designer.cs
@@ -0,0 +1,194 @@
+﻿
+namespace Calculate2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.Numbera = new System.Windows.Forms.TextBox();
+            this.Numberb = new System.Windows.Forms.TextBox();
+            this.NumberAnwser = new System.Windows.Forms.TextBox();
+            this.Add = new System.Windows.Forms.Button();
+            this.Minus = new System.Windows.Forms.Button();
+            this.Multiply = new System.Windows.Forms.Button();
+            this.Divide = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(27, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 18);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "计算器";
+            // 
+            // Numbera
+            // 
+            this.Numbera.Location = new System.Drawing.Point(30, 102);
+            this.Numbera.Name = "Numbera";
+            this.Numbera.Size = new System.Drawing.Size(100, 28);
+            this.Numbera.TabIndex = 1;
+            // 
+            // Numberb
+            // 
+            this.Numberb.Location = new System.Drawing.Point(231, 102);
+            this.Numberb.Name = "Numberb";
+            this.Numberb.Size = new System.Drawing.Size(100, 28);
+            this.Numberb.TabIndex = 2;
+            // 
+            // NumberAnwser
+            // 
+            this.NumberAnwser.Location = new System.Drawing.Point(628, 102);
+            this.NumberAnwser.Name = "NumberAnwser";
+            this.NumberAnwser.Size = new System.Drawing.Size(100, 28);
+            this.NumberAnwser.TabIndex = 3;
+            // 
+            // Add
+            // 
+            this.Add.Location = new System.Drawing.Point(453, 92);
+            this.Add.Name = "Add";
+            this.Add.Size = new System.Drawing.Size(75, 23);
+            this.Add.TabIndex = 4;
+            this.Add.Text = "+";
+            this.Add.UseVisualStyleBackColor = true;
+            this.Add.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Minus
+            // 
+            this.Minus.Location = new System.Drawing.Point(453, 131);
+            this.Minus.Name = "Minus";
+            this.Minus.Size = new System.Drawing.Size(75, 23);
+            this.Minus.TabIndex = 5;
+            this.Minus.Text = "-";
+            this.Minus.UseVisualStyleBackColor = true;
+            this.Minus.Click += new System.EventHandler(this.Minus_Click);
+            // 
+            // Multiply
+            // 
+            this.Multiply.Location = new System.Drawing.Point(453, 174);
+            this.Multiply.Name = "Multiply";
+            this.Multiply.Size = new System.Drawing.Size(75, 23);
+            this.Multiply.TabIndex = 6;
+            this.Multiply.Text = "*";
+            this.Multiply.UseVisualStyleBackColor = true;
+            this.Multiply.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Divide
+            // 
+            this.Divide.Location = new System.Drawing.Point(453, 216);
+            this.Divide.Name = "Divide";
+            this.Divide.Size = new System.Drawing.Size(75, 23);
+            this.Divide.TabIndex = 7;
+            this.Divide.Text = "/";
+            this.Divide.UseVisualStyleBackColor = true;
+            this.Divide.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 61);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(152, 18);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "请输入第一个数字";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(213, 61);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(152, 18);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "请输入第二个数字";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(436, 60);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(116, 18);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "请选择运算符";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(655, 60);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(44, 18);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "结果";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Divide);
+            this.Controls.Add(this.Multiply);
+            this.Controls.Add(this.Minus);
+            this.Controls.Add(this.Add);
+            this.Controls.Add(this.NumberAnwser);
+            this.Controls.Add(this.Numberb);
+            this.Controls.Add(this.Numbera);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox Numbera;
+        private System.Windows.Forms.TextBox Numberb;
+        private System.Windows.Forms.TextBox NumberAnwser;
+        private System.Windows.Forms.Button Add;
+        private System.Windows.Forms.Button Minus;
+        private System.Windows.Forms.Button Multiply;
+        private System.Windows.Forms.Button Divide;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
